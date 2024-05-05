3fce1d66feaf656bef5f4ca3c67377cdcfd88f08
cs <cs@jazz>
2024-05-05 17:58:45 +0200

    remove commented code, rename var ggcu to ggc

--- lua/insgitheader/helper/get-git-config-user.lua
+++ lua/insgitheader/helper/get-git-config-user.lua
@@ -5,16 +5,14 @@ function M.get_git_user(item)
 	local result = handle:read("*l")
 	handle:close()
 	if result ~= nil then
-		-- print(result)
 		return result
 	else
 		if item == "email" then
-			-- print("unknown@example.org")
 			return "unknown@example.org"
 		else
-			-- print("unknown")
 			return "unknown"
 		end
 	end
 end
+
 return M
--- lua/insgitheader/init.lua
+++ lua/insgitheader/init.lua
@@ -1,54 +1,23 @@
 local M = {}
 
-local ggcu = require("insgitheader.helper.get-git-config-user")
+local ggc = require("insgitheader.helper.get-git-config-user")
 
 local settings = {
-	name = ggcu.get_git_user("name"),
-	email = ggcu.get_git_user("email"),
+	name = ggc.get_git_user("name"),
+	email = ggc.get_git_user("email"),
 }
 
--- if settings.name == "" then
--- 	settings.name = "unknown"
--- end
---
--- if settings.email == "" then
--- 	settings.email = "unknown@example.com"
--- end
-
--- M.setup = function(optstable)
 M.setup = function(opts)
 	opts = opts or {}
-	-- local ggcu = require("insgitheader.helper.get-git-config-user")
 	if opts.name ~= nil then
 		settings.name = opts.name
-		-- else
-		-- 	settings.name = ggcu.get_git_user("name")
 	end
 	if opts.email ~= nil then
 		settings.email = opts.email
-		-- else
-		-- 	settings.email = "<" .. ggcu.get_git_user("email") .. ">"
 	end
 end
 
 M.insert_headers = function()
-	-- require("insgitheader").setup()
-	-- vim.api.nvim_create_user_command("InsGitHeader", function()
-	-- local name
-	-- local ggcu = require("insgitheader.helper.get-git-config-user")
-	-- if not settings.name then
-	-- name = opts.name
-	-- else
-	-- local ggn = require("insgitheader.helper.get-git-user-name")
-	-- name = ggcu.get_git_user("name")
-	-- end
-	-- local email
-	-- if opts.email then
-	-- email = "<" .. opts.email .. ">"
-	-- else
-	-- local gge = require("insgitheader.helper.get-git-user-email")
-	-- email = "<" .. ggcu.get_git_user("email") .. ">"
-	-- end
 	-- get comment signs
 	local cleft, cright
 	local gcc = require("insgitheader.helper.get-comment-chars")
